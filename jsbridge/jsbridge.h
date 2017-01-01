056d56ed9810f1ff7b6bcd0dbb55a0486daf30d9
wen.du <wen.du@yingzt.com>
2017-01-01 12:43:46 +0800

    Initial Commit

---
+++ jsbridge/jsbridge.h
@@ -0,0 +1,13 @@
+//
+//  jsbridge.h
+//  jsbridge
+//
+//  Created by 杜文 on 17/1/1.
+//  Copyright © 2017年 杜文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsbridge : NSObject
+
+@end
---
+++ jsbridge/jsbridge.m
@@ -0,0 +1,13 @@
+//
+//  jsbridge.m
+//  jsbridge
+//
+//  Created by 杜文 on 17/1/1.
+//  Copyright © 2017年 杜文. All rights reserved.
+//
+
+#import "jsbridge.h"
+
+@implementation jsbridge
+
+@end
